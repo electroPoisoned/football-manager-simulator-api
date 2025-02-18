d275e62fded30282684ae080e31c61193470f1e7
electroPoisoned <154272666+electroPoisoned@users.noreply.github.com>
2025-02-18 11:55:11 +0300

    init

---
+++ src/main/java/by/electropoisoned/football_manager_simulator_api/FootballManagerSimulatorApiApplication.java
@@ -0,0 +1,13 @@
+package by.electropoisoned.football_manager_simulator_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballManagerSimulatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballManagerSimulatorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/electropoisoned/football_manager_simulator_api/FootballManagerSimulatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package by.electropoisoned.football_manager_simulator_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballManagerSimulatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
